31a442bc9557bc81b47488be8ccd97d75fbf5445
André Cancella de Paula <siscancella@gmail.com>
2024-04-28 23:34:07 -0300

    clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,6 +32,7 @@ const dbConfig: TypeOrmModuleOptions = {
         store: redisStore,
         host: process.env.REDIS_URL,
         port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD
       }),
     }),
     ],
